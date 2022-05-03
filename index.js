a2813399dbd8bceb122e838430d6db8dc474f9b4
joao99lennu <101157554+joao99lennu@users.noreply.github.com>
2022-05-02 21:52:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+$("#btnLogin").click(
+    function(){
+        login=
+        {
+            'usuario' : document.getElementById('inputEmail').Value,
+            'senha' : document.getElementById('inputEmail').value
+
+        };
+
+    console.log("Essa é uma mensagem de log");
+    console.log(
+        document.getElementById('inputEmail').value
+
+    );
+
+    console.log(login);
+    
+    )
+        
---
+++ script/index.js
@@ -0,0 +1,31 @@
+$("#btnLogin").click(
+    function(){
+        $.ajax({
+            type='',
+            url=''
+            success: function(data){
+
+            },
+            error:function(data){
+                
+            }
+
+        })
+       // login =
+        //{
+            //'usuario' :  document.getElementById('inputEmail').value,
+           // 'senha' :  document.getElementById('inputEmail').value
+        
+        //};
+
+        //if(login.senha == '1234'){
+           // document.getElementById('resultado').innerHTML= '<b>bem vindo</b>'
+           // alert("bem-vindo");
+        //} else{
+           // document.
+           // alert("usuario ou senha errada")
+        //}
+        
+       
+    //}
+)
